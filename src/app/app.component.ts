7daff58201a1428a208259d90c32751686e2bc1a
Meza, Javier <javier.meza@thermofisher.com>
2022-07-07 22:48:11 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cc-admin-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'catalog-counts-admin-web';
+}
